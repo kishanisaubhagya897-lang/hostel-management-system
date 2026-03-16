b2f68812fd0fb8755bf44ebf5385c3cceaae3cc0
Banu_RB <rushdahbanu313@gmail.com>
2026-03-16 16:21:28 +0530

    Add multer configuration and upload assets for team members and images

---
+++ backend/config/multer.js
@@ -0,0 +1,15 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/"); // folder you want to save files
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  }
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
